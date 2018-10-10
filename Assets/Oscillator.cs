35e76bdf13ad9e838bd7250df73f37f6660f7581
Neddymond <Neddymond10@gmail.com>
2018-10-10 17:58:27 +0100

    Added an Autotamatic oscillation to the game objects

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f );
+    [SerializeField] float period = 2f;
 
-    [Range(0, 1)] [SerializeField] float movementFactor;
-
+     float movementFactor; // 0 for not moved, 1 for fully moved
     Vector3 startingPos;
 	// Use this for initialization
 	void Start () {
@@ -16,6 +16,13 @@ public class Oscillator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //set movement factor
+        float cycles = Time.time/period; //grows conitnually from 0
+
+        const float tau = Mathf.PI * 2f; //about 6.28
+        float RawSineWave = Mathf.Sin(cycles * tau); //goes from -1 to +1
+
+        movementFactor = RawSineWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
 
